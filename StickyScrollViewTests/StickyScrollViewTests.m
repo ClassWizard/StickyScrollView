4767ea6ff7fa809248795eb38fa28b82809d6751
ClassWizard <chenlongwei@dz.tt>
2016-03-21 13:46:10 +0800

    Initial Commit

---
+++ StickyScrollViewTests/StickyScrollViewTests.m
@@ -0,0 +1,39 @@
+//
+//  StickyScrollViewTests.m
+//  StickyScrollViewTests
+//
+//  Created by ClassWizard on 16/3/21.
+//  Copyright © 2016年 ClassWizard. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StickyScrollViewTests : XCTestCase
+
+@end
+
+@implementation StickyScrollViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
