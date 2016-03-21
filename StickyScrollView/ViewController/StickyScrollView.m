26c8c5436940b2c2b40dcedfd208be066b63aeb0
ClassWizard <chenlongwei@dz.tt>
2016-03-22 02:14:13 +0800

    关闭scrollView的自动布局subViews

--- StickyScrollView/ViewController/StickyScrollView.m
+++ StickyScrollView/ViewController/StickyScrollView.m
@@ -35,37 +35,54 @@
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
     NSLog(@"%@",NSStringFromCGPoint(scrollView.contentOffset));
     
-    if ((scrollView.contentOffset.y + 64) < CGRectGetMinY(self.stickyViewOne.frame)) {
+    if ((scrollView.contentOffset.y) < CGRectGetMinY(self.stickyViewOne.frame)) {
         NSLog(@"top");
         //销毁第一个悬浮框
         [self.viewOne removeFromSuperview];
-        self.viewOne = nil;
         //显示stickyView;
         self.stickyViewOne.hidden = NO;
     }
-    else if (scrollView.contentOffset.y + 64 < CGRectGetMaxY(self.stickyViewOne.frame)) {
+    else if (scrollView.contentOffset.y < CGRectGetMaxY(self.stickyViewOne.frame)) {
         //第一个悬浮框开始
         NSLog(@"fist begin");
+        //右移
         CGRect frame = self.viewOne.frame;
-        frame.origin.x = scrollView.contentOffset.y + 64 - CGRectGetMinY(self.stickyViewOne.frame);
+        frame.origin.x = scrollView.contentOffset.y - CGRectGetMinY(self.stickyViewOne.frame);
         self.viewOne.frame = frame;
         [self.view addSubview:self.viewOne];
         //隐藏stickyView;
         self.stickyViewOne.hidden = YES;
     }
-    else if (scrollView.contentOffset.y + 64 < CGRectGetMinY(self.stickyViewTwo.frame) - self.viewOne.frame.size.height + self.stickyViewTwo.frame.size.height) {
+    else if (scrollView.contentOffset.y < CGRectGetMaxY(self.stickyViewTwo.frame) - self.viewOne.frame.size.height) {
+        //将第一个悬浮框设置到最终位置
+        CGRect frame = self.viewOne.frame;
+        frame.origin.x = CGRectGetMaxY(self.stickyViewOne.frame) - CGRectGetMinY(self.stickyViewOne.frame);
+        self.viewOne.frame = frame;
+        
         // 404 = 460 - 100 + 44
         //第二个悬浮框开始
         NSLog(@"中间区域");
+        
+        //销毁第二个悬浮框
+        [self.viewTwo removeFromSuperview];
     }
-    else if (scrollView.contentOffset.y + 64 < CGRectGetMaxY(self.stickyViewTwo.frame) - self.stickyViewTwo.frame.size.height){
+    else if (scrollView.contentOffset.y < CGRectGetMaxY(self.stickyViewTwo.frame) - CGRectGetHeight(self.viewOne.frame) + CGRectGetHeight(self.viewTwo.frame)) {
+        
         // 384 + 64 =
         //上个区域到这个条件为第二个悬浮框区域
         NSLog(@"second begin");
         [self.view addSubview:self.viewTwo];
+        [self.view bringSubviewToFront:self.viewOne];\
+        CGRect frame = self.viewTwo.frame;
+        frame.origin.y = scrollView.contentOffset.y - 404 + CGRectGetMaxY(self.viewOne.frame) - frame.size.height;
+        self.viewTwo.frame = frame;
     }
     else {
         NSLog(@"bottom");
+        //设置第二个悬浮框至终点位置
+        CGRect frame = self.viewTwo.frame;
+        frame.origin.y = CGRectGetMaxY(self.viewOne.frame);
+        self.viewTwo.frame = frame;
     }
 }
 
